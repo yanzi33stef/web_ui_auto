16a00c873854b113707d8b700c49fbaec64c8605
jing <16948850@qq.com>
2020-06-24 18:35:14 +0800

    update index_data.py error_data

--- data/index_data.py
+++ data/index_data.py
@@ -4,6 +4,6 @@
 # @Author   :  Ella
 
 course_code_error = [
-    {"course_code": "P69UVV", "expected": "你已经选过此课程"},
+    {"course_code": "P69UVV", "expected": "已经选过此课程"},
     {"course_code": "123", "expected": "加课验证码必须是6位字符"},
 ]
